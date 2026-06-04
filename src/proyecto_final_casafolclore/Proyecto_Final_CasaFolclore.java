9d6131f577efc4325c4454054eebe80d7c5f688f
NYGS <N00199817@upn.pe>
2026-06-04 16:04:53 -0500

    prueba

--- src/proyecto_final_casafolclore/Proyecto_Final_CasaFolclore.java
+++ src/proyecto_final_casafolclore/Proyecto_Final_CasaFolclore.java
@@ -16,7 +16,7 @@ public class Proyecto_Final_CasaFolclore {
     public static void main(String[] args) {
         // TODO code application logic here
     //holaaaaaaaaaaaa
-        System.out.println("Holaaa");
+        System.out.println("Hola");
     }
     
 }
