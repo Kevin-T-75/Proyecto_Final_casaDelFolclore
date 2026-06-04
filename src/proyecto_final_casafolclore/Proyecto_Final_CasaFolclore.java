bfe0b86679757f704c578beb757c8994a2e39a7f
KevinTasilla <n00444846@upn.pe>
2026-06-04 15:22:08 -0500

    primer holaa

---
+++ src/proyecto_final_casafolclore/Proyecto_Final_CasaFolclore.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_final_casafolclore;
+
+/**
+ *
+ * @author OS
+ */
+public class Proyecto_Final_CasaFolclore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
