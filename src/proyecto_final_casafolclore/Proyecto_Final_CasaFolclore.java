f8ff41b7ba30745a36b1734eb9fd1379e46a75b2
KevinTasilla <n00444846@upn.pe>
2026-06-04 16:30:04 -0500

    Clases K

--- src/proyecto_final_casafolclore/Proyecto_Final_CasaFolclore.java
+++ src/proyecto_final_casafolclore/Proyecto_Final_CasaFolclore.java
@@ -15,6 +15,9 @@ public class Proyecto_Final_CasaFolclore {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        System.out.println("helooooooo");
     }
     
 }
