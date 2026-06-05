0c5c373b9d1e1c1bbbb2c7f129a3ddd5a4be95da
sahir Bazan <sahirbazan544@gmail.com>
2026-06-05 02:16:39 -0500

    mamain

--- src/proyecto_final_casafolclore/Proyecto_Final_CasaFolclore.java
+++ src/proyecto_final_casafolclore/Proyecto_Final_CasaFolclore.java
@@ -16,116 +16,306 @@ public class Proyecto_Final_CasaFolclore {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try ( // TODO code application logic here
-                Scanner sc = new Scanner(System.in)) {
-            Administrador admin = new Administrador(
-                    "Gerente",
-                    "U001",
-                    "Carlos Ramos",
-                    "carlos@gmail.com",
-                    "12345"
-            );
-            
-            Cliente cliente1 = new Cliente(
-                    "987654321",
-                    "Cajamarca",
-                    "Frecuente"
-            );
-            
-            Traje traje1 = new Traje(
-                    "T001",
-                    "Traje de Marinera",
-                    "Marinera Norteña",
-                    "M",
-                    true,
-                    "80"
-            );
-            
-            Pago pago1 = new Pago(
-                    1,
-                    new Date(),
-                    80.00,
-                    "Yape"
-            );
-            
-            Alquiler alquiler1 = new Alquiler(
-                    1001,
-                    "05/06/2026",
-                    "10/06/2026",
-                    80.00,
-                    "Pendiente",
-                    cliente1,
-                    pago1
-            );
-            
-            ReporteAdministrador rep1 = new ReporteAdministrador(
-                    "R001",
-                    new Date(),
-                    "Reporte de alquileres"
-            );
-            
-            admin.iniciarSesion();
-            
-            int opcion;
-            
-            do {
-                System.out.println("\n LA CASA DEL FOLCLORE ");
-                System.out.println("1. Registrar cliente");
-                System.out.println("2. Registrar traje");
-                System.out.println("3. Registrar alquiler");
-                System.out.println("4. Registrar pago");
-                System.out.println("5. Consultar inventario");
-                System.out.println("6. Generar reporte");
-                System.out.println("7. Cerrar sesión");
-                System.out.print("Seleccione una opción: ");
-                
-                opcion = sc.nextInt();
-                
-                switch (opcion) {
-                    
-                    case 1 -> {
-                        System.out.println("\nCLIENTE");
-                        System.out.println("Telefono: " + cliente1.getTelefono_Cliente());
-                        System.out.println("Direccion: " + cliente1.getDireccion_Cliente());
-                        System.out.println("Tipo: " + cliente1.getTipo_Cliente());
-                        admin.gestionarClientes();
-                    }
-                        
-                    case 2 -> {
-                        System.out.println("\nTRAJE");
-                        System.out.println("Codigo: " + traje1.getIdTraje());
-                        System.out.println("Nombre: " + traje1.getNombre_Traje());
-                        admin.gestionarTrajes();
-                    }
-                        
-                    case 3 -> alquiler1.registrarAlquiler();
-                        
-                    case 4 -> pago1.registrarPago();
-                        
-                    case 5 -> {
-                        System.out.println("\nINVENTARIO");
-                        System.out.println("Codigo: " + traje1.getIdTraje());
-                        System.out.println("Nombre: " + traje1.getNombre_Traje());
-                        System.out.println("Estado: " + traje1.consultarDisponible());
-                        System.out.println("Precio: S/. " + traje1.getPrecioAlquiler());
-                    }
-                        
-                    case 6 -> {
-                        rep1.generarReporte();
-                        admin.gestionarReporte(rep1);
-                        admin.mostrarReportes();
-                    }
-                        
-                    case 7 -> {
-                        admin.cerrarSesion();
-                        System.out.println("Sesión finalizada.");
-                    }
-                        
-                    default -> System.out.println("Opción no válida.");
-                }
-                
-            } while (opcion != 7);
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("1. Iniciar como Administrador");
+    System.out.println("2. Iniciar como Cliente");
+    System.out.print("Seleccione una opción: ");
+
+    int tipoUsuario = sc.nextInt();
+    sc.nextLine();
+
+    if (tipoUsuario == 1) {
+
+        System.out.println("\n--- DATOS DEL ADMINISTRADOR ---");
+
+        System.out.print("ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Correo: ");
+        String correo = sc.nextLine();
+
+        System.out.print("Contraseña: ");
+        String contraseña = sc.nextLine();
+
+        System.out.print("Cargo: ");
+        String cargo = sc.nextLine();
+
+        Administrador admin = new Administrador(
+                cargo,
+                id,
+                nombre,
+                correo,
+                contraseña
+        );
+
+        admin.iniciarSesion();
+
+        int opcion;
+
+        do {
+
+        System.out.println("\n===== MENÚ ADMINISTRADOR =====");
+        System.out.println("1. Registrar Cliente");
+        System.out.println("2. Registrar Traje");
+        System.out.println("3. Registrar Alquiler");
+        System.out.println("4. Registrar Pago");
+        System.out.println("5. Consultar Inventario");
+        System.out.println("6. Generar Reporte");
+        System.out.println("7. Cerrar Sesión");
+        System.out.print("Seleccione una opción: ");
+
+        opcion = sc.nextInt();
+        sc.nextLine();
+
+        switch (opcion) {
+
+            case 1:
+
+                System.out.println("\n--- REGISTRO CLIENTE ---");
+
+                System.out.print("Teléfono: ");
+                String telefono = sc.nextLine();
+
+                System.out.print("Dirección: ");
+                String direccion = sc.nextLine();
+
+                System.out.print("Tipo de cliente: ");
+                String tipoCliente = sc.nextLine();
+
+                Cliente cliente = new Cliente(
+                        telefono,
+                        direccion,
+                        tipoCliente
+                );
+
+                System.out.println("Cliente registrado correctamente.");
+                admin.gestionarClientes();
+
+                break;
+
+            case 2:
+
+                System.out.println("\n--- REGISTRO TRAJE ---");
+
+                System.out.print("Código: ");
+                String idTraje = sc.nextLine();
+
+                System.out.print("Nombre: ");
+                String nombreTraje = sc.nextLine();
+
+                System.out.print("Danza: ");
+                String danza = sc.nextLine();
+
+                System.out.print("Talla: ");
+                String talla = sc.nextLine();
+
+                System.out.print("Precio alquiler: ");
+                String precio = sc.nextLine();
+
+                Traje traje = new Traje(
+                        idTraje,
+                        nombreTraje,
+                        danza,
+                        talla,
+                        true,
+                        precio
+                );
+
+                System.out.println("Traje registrado correctamente.");
+                admin.gestionarTrajes();
+
+              
+
+            case 3:
+
+                System.out.println("\n--- REGISTRO ALQUILER ---");
+
+                System.out.print("ID alquiler: ");
+                int idAlquiler = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("Fecha inicio: ");
+                String fechaInicio = sc.nextLine();
+
+                System.out.print("Fecha fin: ");
+                String fechaFin = sc.nextLine();
+
+                System.out.print("Monto: ");
+                double monto = sc.nextDouble();
+                sc.nextLine();
+
+                Cliente clienteAlquiler = new Cliente(
+                        "999999999",
+                        "Sin dirección",
+                        "Normal"
+                );
+
+                Pago pagoAlquiler = new Pago(
+                        1,
+                        new Date(),
+                        monto,
+                        "Pendiente"
+                );
+
+                Alquiler alquiler = new Alquiler(
+                        idAlquiler,
+                        fechaInicio,
+                        fechaFin,
+                        monto,
+                        "ACTIVO",
+                        clienteAlquiler,
+                        pagoAlquiler
+                );
+
+                alquiler.registrarAlquiler();
+
+                break;
+
+            case 4:
+
+                System.out.println("\n--- REGISTRO PAGO ---");
+
+                System.out.print("ID Pago: ");
+                int idPago = sc.nextInt();
+
+                System.out.print("Monto: ");
+                double montoPago = sc.nextDouble();
+                sc.nextLine();
+
+                System.out.print("Método de pago: ");
+                String metodo = sc.nextLine();
+
+                Pago pago = new Pago(
+                        idPago,
+                        new Date(),
+                        montoPago,
+                        metodo
+                );
+
+                pago.registrarPago();
+
+                break;
+
+            case 5:
+
+                System.out.println("\n--- CONSULTA INVENTARIO ---");
+                System.out.println("Funcionalidad disponible.");
+                break;
+
+            case 6:
+
+                ReporteAdministrador reporte =
+                        new ReporteAdministrador(
+                                "R001",
+                                new Date(),
+                                "Reporte General"
+                        );
+
+                reporte.generarReporte();
+                admin.gestionarReporte(reporte);
+                admin.mostrarReportes();
+
+                break;
+
+            case 7:
+
+                admin.cerrarSesion();
+                System.out.println("Sesión finalizada.");
+                break;
+
+            default:
+
+                System.out.println("Opción inválida.");
+
         }
+
+    } while (opcion != 7);
+
+    } else if (tipoUsuario == 2) {
+
+        System.out.println("\n--- DATOS DEL CLIENTE ---");
+
+        System.out.print("Teléfono: ");
+        String telefono = sc.nextLine();
+
+        System.out.print("Dirección: ");
+        String direccion = sc.nextLine();
+
+        System.out.print("Tipo de cliente: ");
+        String tipoCliente = sc.nextLine();
+
+        Cliente cliente = new Cliente(
+                telefono,
+                direccion,
+                tipoCliente
+        );
+
+        int opcion;
+
+        do {
+
+            System.out.println("\n===== MENÚ CLIENTE =====");
+            System.out.println("1. Ver Datos");
+            System.out.println("2. Reservar");
+            System.out.println("3. Alquilar");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione: ");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+
+            case 1:
+
+                System.out.println("Teléfono: "
+                        + cliente.getTelefono_Cliente());
+
+                System.out.println("Dirección: "
+                        + cliente.getDireccion_Cliente());
+
+                System.out.println("Tipo: "
+                        + cliente.getTipo_Cliente());
+
+            break;
+
+                case 2:
+
+                    cliente.reserva();
+                    System.out.println("Reserva realizada.");
+                    break;
+
+                case 3:
+
+                    cliente.alquilar();
+                    System.out.println("Alquiler realizado.");
+                    break;
+
+                case 4:
+
+                    System.out.println("Gracias por usar el sistema.");
+                    break;
+
+                default:
+
+                    System.out.println("Opción inválida.");
+
+            }
+
+        } while (opcion != 4);
+
+    } else {
+
+        System.out.println("Opción de usuario inválida.");
+
+    }
+
+     sc.close();
     }
 }
 
