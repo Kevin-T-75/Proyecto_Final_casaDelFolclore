b6c7891709163afd9c42e671542576df63d91a8a
NYGS <N00199817@upn.pe>
2026-06-27 16:28:40 -0500

    agregando atributos en Persona

--- src/proyecto_final_casafolclore/Clases/Persona.java
+++ src/proyecto_final_casafolclore/Clases/Persona.java
@@ -10,33 +10,74 @@ package proyecto_final_casafolclore.Clases;
  */
 public abstract class Persona {
     
-    protected String idUsuario;
-    protected  String nombre_Usuario; 
-    protected  String correo_Usuario; 
+    protected String idPersona;
+    protected  String nombre; 
+    protected  String apellidoPaterno; 
+    protected  String apellidoMaterno; 
+    protected  String tipoDocumento; 
+    protected  String nroDocumento; 
     protected  String contraseña_Usuario; 
+    protected  String correo_Usuario; 
 
-    public String getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(String idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getNombre_Usuario() {
-        return nombre_Usuario;
-    }
-
-    public void setNombre_Usuario(String nombre_Usuario) {
-        this.nombre_Usuario = nombre_Usuario;
-    }
-
-    public String getCorreo_Usuario() {
-        return correo_Usuario;
-    }
-
-    public void setCorreo_Usuario(String correo_Usuario) {
+    public Persona() {}
+    
+    public Persona(String idPersona, String nombre, String apellidoPaterno, String apellidoMaterno, String tipoDocumento, String nroDocumento, String contraseña_Usuario, String correo_Usuario) {
+        this.idPersona = idPersona;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.tipoDocumento = tipoDocumento;
+        this.nroDocumento = nroDocumento;
+        this.contraseña_Usuario = contraseña_Usuario;
         this.correo_Usuario = correo_Usuario;
+    }
+
+    public String getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(String idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNroDocumento() {
+        return nroDocumento;
+    }
+
+    public void setNroDocumento(String nroDocumento) {
+        this.nroDocumento = nroDocumento;
     }
 
     public String getContraseña_Usuario() {
@@ -47,12 +88,16 @@ public abstract class Persona {
         this.contraseña_Usuario = contraseña_Usuario;
     }
 
-    public Persona(String idUsuario, String nombre_Usuario, String correo_Usuario, String contraseña_Usuario) {
-        this.idUsuario = idUsuario;
-        this.nombre_Usuario = nombre_Usuario;
-        this.correo_Usuario = correo_Usuario;
-        this.contraseña_Usuario = contraseña_Usuario;
+    public String getCorreo_Usuario() {
+        return correo_Usuario;
     }
+
+    public void setCorreo_Usuario(String correo_Usuario) {
+        this.correo_Usuario = correo_Usuario;
+    }
+    
+    
+   
 
     public abstract void iniciarSesion();    
     
